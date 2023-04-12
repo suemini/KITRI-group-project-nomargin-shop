67cde5a4401aa57f3460b17feb35ba12b264a70b
suemini <say5291@naver.com>
2023-04-12 13:19:46 +0900

    2023.04.12 11:45 Commit head, header, footer 반복 include 추가

--- nomargin.com/nomargin/src/main/java/EZ/nomargin/domain/item/ItemType.java
+++ nomargin.com/nomargin/src/main/java/EZ/nomargin/domain/item/ItemType.java
@@ -5,6 +5,8 @@ public enum ItemType {
     Top("상의"),
     Bottom("하의"),
     Outer("외투");
+
+    
     private final String description;
     ItemType(String description){
         this.description = description;
