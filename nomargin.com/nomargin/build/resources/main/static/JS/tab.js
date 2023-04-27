d5ee4bba3b0b8a1959c0584e2bf0346f0dee258e
suemini <say5291@naver.com>
2023-04-27 16:04:20 +0900

    2023-04-27 16:03 by 정은 마이페이지 추가, item.html 기능 일부 수정

--- nomargin.com/nomargin/build/resources/main/static/JS/tab.js
+++ nomargin.com/nomargin/build/resources/main/static/JS/tab.js
@@ -2,24 +2,24 @@ $(document).ready(function () {
     $('a[href="#item-desc"]').click(function () {
         $('html, body').animate({
             scrollTop: $(".item-subImages").offset().top
-        }, 1000);
+        }, 500);
     });
 
     $('a[href="#item-comments"]').click(function () {
         $('html, body').animate({
             scrollTop: $(".item-comments").offset().top
-        }, 1000);
+        }, 500);
     });
 
-    $('a[href="#item-rating"]').click(function () {
-        $('html, body').animate({
-            scrollTop: $(".item-rating").offset().top
-        }, 1000);
-    });
+    // $('a[href="#item-rating"]').click(function () {
+    //     $('html, body').animate({
+    //         scrollTop: $(".item-rating").offset().top
+    //     }, 1000);
+    // });
 
     $('a[href="#item-seller"]').click(function () {
         $('html, body').animate({
-            scrollTop: $(".seller-info").offset().top
-        }, 1000);
+            scrollTop: $(".seller").offset().top
+        }, 500);
     });
 });
